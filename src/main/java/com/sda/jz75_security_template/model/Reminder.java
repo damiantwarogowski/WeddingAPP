d8799afa3aed6b62056dfcdac29eaa07417bc567
DamianT <damian.twarogowski@gmail.com>
2021-07-07 21:18:10 +0200

    Dane o uzytkowniku wyswietlaja sie

--- src/main/java/com/sda/jz75_security_template/model/Reminder.java
+++ src/main/java/com/sda/jz75_security_template/model/Reminder.java
@@ -1,20 +1,29 @@
 package com.sda.jz75_security_template.model;
 
+import jdk.jfr.Timespan;
+import jdk.jfr.Timestamp;
+import net.bytebuddy.implementation.bind.annotation.RuntimeType;
 import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.persistence.Basic;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.time.LocalDate;
 
-public class Payment {
+public class Reminder {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private double paymentAmount;
-
     @DateTimeFormat(pattern = "yyyy-MM-dd")
-    private LocalDate datePaymentAmount;
+    private LocalDate reminderDay;
+
+    @Basic
+    private java.sql.Time reminderTime;
+
+    private String contentTxt;
+
+
 }
