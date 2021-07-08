64018a3d0f684e5ffb35c4b881579f823c89260d
DamianT <damian.twarogowski@gmail.com>
2021-07-08 20:21:12 +0200

    Dane o uzytkowniku wyswietlaja sie

---
+++ src/main/java/com/sda/jz75_security_template/model/dto/PersonDto.java
@@ -0,0 +1,26 @@
+package com.sda.jz75_security_template.model.dto;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@ApiModel(value = "Basic Person information.",
+        description = "This model contains information about student.")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PersonDto {
+    @ApiModelProperty(value = "First name.", example = "JAN")
+    private String name;
+
+    @ApiModelProperty(value = "Last name.", example = "KOWALSKI")
+    private String surname;
+
+    @ApiModelProperty(value = "email.", example = "JAN.KOWALSKI@GMAIL.COM")
+    private String email;
+
+}
+
