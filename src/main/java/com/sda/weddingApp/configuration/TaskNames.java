309f6e4583f9bbe16eb98e6a34b48e8a1a5da882
Sabina <sabina-lange@wp.pl>
2021-07-26 20:12:39 +0200

    Task names

--- src/main/java/com/sda/weddingApp/configuration/TaskNames.java
+++ src/main/java/com/sda/weddingApp/configuration/TaskNames.java
@@ -13,18 +13,18 @@ public interface TaskNames {
     public final static String TASK_PHOTOGRAPHER = "Booking a photographer";
     public final static String TASK_CAMERAMAN = "Booking a cameraman";
 
-    public final static String TASK_RINGS1 = "Buying a ring by first person";
-    public final static String TASK_OUTFIT1 = "Buying a wedding outfit by first person ";
-    public final static String TASK_HAIRDRESSER1  = "Making a wedding hairstyle at the hairdresser by first person";
-    public final static String TASK_BARBER1  = "Making a wedding hairstyle at the barber by first person";
-    public final static String TASK_MAKEUP_ARTIST1  = "Going to makeup artist by first person";
-    public final static String TASK_MANI_PEDI_CURE1  = "Going to the beautician for mani/pedicure by first person";
+    public final static String TASK_RINGS1 = "Person 1: Buying a ring";
+    public final static String TASK_OUTFIT1 = "Person 1: Buying a wedding outfit";
+    public final static String TASK_HAIRDRESSER1  = "Person 1: Making a wedding hairstyle at the hairdresser";
+    public final static String TASK_BARBER1  = "Person 1: Making a wedding hairstyle at the barber";
+    public final static String TASK_MAKEUP_ARTIST1  = "Person 1 : Going to makeup artist";
+    public final static String TASK_MANI_PEDI_CURE1  = "Person 1: Going to the beautician for mani/pedicure";
 
-    public final static String TASK_RINGS2 = "Buying a ring by secound person";
-    public final static String TASK_OUTFIT2 = "Buying a wedding outfit by secound person";
-    public final static String TASK_HAIRDRESSER2  = "Making a wedding hairstyle at the hairdresser by secound person";
-    public final static String TASK_BARBER2  = "Making a wedding hairstyle at the barber by secound person";
-    public final static String TASK_MAKEUP_ARTIST2  = "Going to makeup artist by secound person";
-    public final static String TASK_MANI_PEDI_CURE2  = "Going to the beautician for mani/pedicure by secound person";
+    public final static String TASK_RINGS2 = "Person 2: Buying a ring";
+    public final static String TASK_OUTFIT2 = "Person 2: Buying a wedding outfit";
+    public final static String TASK_HAIRDRESSER2  = "Person 2: Making a wedding hairstyle at the hairdresser";
+    public final static String TASK_BARBER2  = "Person 2: Making a wedding hairstyle at the barber";
+    public final static String TASK_MAKEUP_ARTIST2  = "Person 2: Going to makeup artist";
+    public final static String TASK_MANI_PEDI_CURE2  = "Person 2: Going to the beautician for mani/pedicure";
 
 }
